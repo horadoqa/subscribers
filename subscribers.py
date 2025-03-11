dbb1e3c632716e10337c700224a99b6e2286feda
Hora do QA <horadoqa@gmail.com>
2025-03-11 20:42:27 -0300

    Remove test

--- subscribers.py
+++ subscribers.py
@@ -160,17 +160,9 @@ def main():
     with open("subscribers_updates.json", "r") as f:
         subscribers_updates = json.load(f)
 
-    print(subscribers_updates)
-
     last_subscriber_count = [
         update['SubscribersCount'] for update in subscribers_updates['SubscribersUpdates']
-    ]
-    
-    print(last_subscriber_count)
-
-    last_subscriber_count = last_subscriber_count.sort(reverse=True)[0]['SubscribersCount']
-
-    print(last_subscriber_count)
+    ].sort(reverse=True)[0]
 
     current_subscriber_count = get_subscriber_count()
 
