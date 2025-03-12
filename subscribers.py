df5ad6a948c50b86dc818c32a79351c7fe97a399
Hora do QA <horadoqa@gmail.com>
2025-03-11 21:03:10 -0300

    Update test

--- subscribers.py
+++ subscribers.py
@@ -166,7 +166,8 @@ def main():
     ]
 
     print(f"Last Subscriber Count {last_subscriber_count}")
-    print(f"Last Subscriber Count Reversed {last_subscriber_count.sort(reverse=True)}")
+    last_subscriber_count = last_subscriber_count.sort(reverse=True)
+    print(f"Last Subscriber Count Reversed {last_subscriber_count}")
 
     current_subscriber_count = get_subscriber_count()
 
